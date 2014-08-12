2c17cdd4e6edaf56ce29146c9701b55dd066c513
Jeff Christy <jeffwchristy@gmail.com>
2014-08-11 23:07:50 -0600

    Remove url from project file

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject buzz-me-when "0.1.0-SNAPSHOT"
   :description "Notify me when a stock reaches a price"
-  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
