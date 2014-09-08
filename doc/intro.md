b006efc84f1278a6b573638f452ca5e0dd3d2093
Jeff Christy <jeffwchristy@gmail.com>
2014-09-07 21:54:57 -0600

    Adding angularjs seed app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to buzz-me-when
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/buzz_me_when/core_test.clj
@@ -0,0 +1,7 @@
+(ns buzz-me-when.core-test
+  (:require [clojure.test :refer :all]
+            [buzz-me-when.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
